0f0c74ae261880eed3545471be054295b6c84cfe
Arthur Elmes <arthur.elmes@gmail.com>
2024-06-24 08:07:57 -0400

    Add sane arg defaults.

--- log_co2.py
+++ log_co2.py
@@ -1,6 +1,6 @@
 import mh_z19 as m19
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -8,9 +8,9 @@ def parse_args():
         description="Log CO2 concentration at given interval for given time period."
         )
     parser.add_argument("-p", "--ping_interval", type=int, help="Number of seconds between sensor reads.", default=30)
-    parser.add_argument("-t0", "--start_datetime", type=lambda date_str: datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"), help="Date in YYYY-MM-DDTHH:MM:SS.ffffff format. Defaults to current time.")
-    parser.add_argument("-t1", "--end_datetime", type=lambda date_str: datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"), help="Date in YYYY-MM-DDTHH:MM:SS.ffffff format. Defaults to one hour from current time.")
-    parser.add_argument("-l", "--log_file_path", type=str, help="Full path to output logfile.")
+    parser.add_argument("-t0", "--start_datetime", type=lambda date_str: datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"), help="Date in YYYY-MM-DDTHH:MM:SS.ffffff format. Defaults to current time.", default=datetime.now())
+    parser.add_argument("-t1", "--end_datetime", type=lambda date_str: datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f"), help="Date in YYYY-MM-DDTHH:MM:SS.ffffff format. Defaults to one hour from current time.", default=datetime.now() + timedelta(hours=1))
+    parser.add_argument("-l", "--log_file_path", type=str, help="Full path to output logfile.", default="co2-log.txt")
 
     args = parser.parse_args()
 
